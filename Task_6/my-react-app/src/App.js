0ea0acb7a608b081b3cb61bf220cc17072eff1e2
BhuvanAsura420 <jastisridhar1401@gmail.com>
2025-02-10 15:06:47 +0530

    Task_6

---
+++ Task_6/my-react-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Hello from "./components/Hello";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to My React App</h1>
+      <Hello name="Japes" />
+    </div>
+  );
+}
+
+export default App;
